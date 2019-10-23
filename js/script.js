d48a22df64a5aa8f8865b5abd3ccd3b9423a42b4
pparmin <philipp.armingeon@googlemail.com>
2019-10-23 15:30:17 +0200

    Significant refactoring of the code and added shader and proper random color functionality

--- js/script.js
+++ js/script.js
@@ -1,84 +1,25 @@
-function getRandom(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+/*
+HOW DOES THIS WORK?
+1. The createGrid function creates an initial drawing surface by assigning the container
+the neccessary styling.
 
-function createGrid(squareNumber, width, height) {
-  const container = document.createElement('div');
-  container.setAttribute('class', 'container');
-  container.style.cssText = `margin: 0 auto; 
-  background-color: rgb(228, 166, 125);
-  width: ${width}px;
-  height: ${height}px;
-  display: grid;
-  cursor: crosshair;
-  box-shadow: -1px 2px 12px 5px rgb(112, 80, 80);
-  grid-template-columns: repeat(${squareNumber}, ${parseInt(height) / squareNumber}px);
-  grid-template-rows: repeat(${squareNumber}, ${parseInt(width) / squareNumber}px);`;
+2. It also creates 32x32 squares. The squares are assigned an event listener that waits for the mouse to enter each square
+and will then call the paintSquare() function.
 
-  console.log(parseInt(height) / squareNumber);
+3. paintSquare has a switch that will use a specific paint mode depending on the mode it is being given.
+The standard mode will be "normal".
 
-  gameWindow.appendChild(container);
+4. All settings buttons that handle a paint mode are assigned a 'click' event listener, which will call the setMode() function
 
-  for (let i = 0; i < squareNumber; i++) {
-    for (let j = 0; j < squareNumber; j++) {
+5. The setMode() function simply assigns a new value to the currentMode variable by pulling it from the clicked element.
 
-      function doPaint(r, g, b) {
-        newDiv.addEventListener('mouseover', (e) => {
-          newDiv.style['background-color'] = `rgb(${r}, ${g}, ${b})`;
-          newDiv.style.transform = 'scale(1.1)';
-        });
+6. Since every square listens to the paintSquare() function, which in turn is dependent on the value of the currentMode variable,
+you have a way to change the modes dynamically. 
 
-        newDiv.addEventListener('mouseout', (e) => {
-          newDiv.style.transform = 'scale(1.0)';
-        });
-      }
+createGrid --> newDiv.addEventListener('mouseenter', paintSquare) --> paintSquare(): What value is currentMode atm? --> change Mode with
+setMode() --> rinse repeat
+*/
 
-      const newDiv = document.createElement('div');
-      newDiv.setAttribute('class', 'item');
-      newDiv.style['text-align'] = 'center';
-      newDiv.style['grid-area'] = `${i} / ${j} / span 1 / span 1`;
-      container.appendChild(newDiv);
-
-      // "Hover" effect
-      newDiv.addEventListener('mouseover', e => {
-
-        newDiv.style['background-color'] = 'rgb(112, 80, 80)';
-        newDiv.style.transform = 'scale(1.1)';
-      });
-
-      newDiv.addEventListener('mouseout', e => {
-        newDiv.style.transform = 'scale(1.0)';
-      });
-
-      // Remove painted surface
-      clearBtn.addEventListener('click', e => {
-        newDiv.style['background-color'] = 'rgb(228, 166, 125)';
-      });
-
-      // Eraser 
-      eraseBtn.addEventListener('click', e => doPaint(228, 166, 125));
-
-      randomBtn.addEventListener('click', () => doPaint(getRandom(255), getRandom(255), getRandom(255)));
-      
-      whiteBtn.addEventListener('click', () => doPaint(255, 255, 255));
-
-      normalBtn.addEventListener('click', () => doPaint(112, 80, 80));
-    }
-  }
-
-  newBtn.addEventListener('click', e => {
-
-    gameWindow.removeChild(container);
-
-    newSquareNumber = prompt("Choose the size of the matrix: (Max 64)");
-    if (newSquareNumber > 64) {
-      alert("Please choose a number smaller than or equal to 64");
-      createGrid(32, 480, 480);
-    } else { 
-      createGrid(newSquareNumber, 480, 480);
-    }
-  });
-}
 
 const clearBtn = document.querySelector('#clear');
 const newBtn = document.querySelector('#new');
@@ -88,5 +29,126 @@ const randomBtn = document.querySelector('#random');
 const whiteBtn = document.querySelector('#white');
 
 const gameWindow = document.querySelector('.game-window');
+const squareNumber = 32;
+const width = 480;
+const height = 480;
+const container = document.createElement('div');
 
-window.onload = createGrid(32, 480, 480); 
+const squares = [];
+let currentMode = "normal";
+
+// Function for creating the Grid
+function createGrid(squareNumber) {
+  container.setAttribute('id', 'container');
+  container.style.cssText = `margin: 0 auto; 
+  background-color: rgb(228, 166, 125);
+  width: ${width}px;
+  height: ${height}px;
+  display: grid;
+  cursor: crosshair;
+  box-shadow: -1px 2px 12px 5px rgb(112, 80, 80);
+  grid-template-columns: repeat(${squareNumber}, ${parseInt(height) / squareNumber}px);
+  grid-template-rows: repeat(${squareNumber}, ${parseInt(width) / squareNumber}px);`;
+  gameWindow.appendChild(container);
+
+  for (let i = 1; i <= squareNumber; i++) {
+    for (let j = 1; j <= squareNumber; j++) {
+      const newDiv = document.createElement('div');
+      newDiv.setAttribute('class', 'item');
+      newDiv.style['text-align'] = 'center';
+      newDiv.style['grid-area'] = `${i} / ${j} / span 1 / span 1`;
+      newDiv.style.opacity = '0.0';
+      //newDiv.style.border =  '.5px dotted grey';
+      container.appendChild(newDiv);
+      console.log(`Created a div with row: ${i} and column: ${j}`);
+      squares.push(newDiv);
+      // puts an event listener on all divs that constantly listens for the paintSquare function on mouseenter
+      newDiv.addEventListener('mouseenter', paintSquare);
+    }
+  }
+}
+
+// The paint function listens to the id value of currentMode that is assigned by clicking the button
+// Depending on each buttons id value, the divs will be colored
+function paintSquare(e) {
+  let opacity = e.target.style.opacity;
+  switch(currentMode) {
+    case ("normal"):
+      e.target.style.opacity = '1.0';
+      e.target.style.backgroundColor = 'rgb(112, 80, 80)';
+    break;
+
+    case ("eraser"):
+      e.target.style.opacity = '1.0';
+      e.target.style.backgroundColor = 'rgb(228, 166, 125)';
+    break;
+
+    case ("random"):
+      e.target.style.opacity = '1.0';
+      e.target.style.backgroundColor = getRandomRGB();
+    break;
+
+    // The shader works by taking the initial value of the event.target (either 0.0 or 1.0)
+    // and continously adding 0.1 to it until it reaches 1.0. 
+    case ("shader"):
+      if(opacity == 1) {
+        return
+      } else {
+        e.target.style.opacity = Number(Math.min(e.target.style.opacity) + 0.1, 1.0);
+        e.target.style.backgroundColor = 'rgb(112, 80, 80)';
+      }
+    break;
+  }
+}
+
+// This function changes the modes by setting the currentMode variable to the id value of the clicked button
+function setMode(e) {
+  console.log(`current mode was: ${currentMode}`);
+  currentMode = e.target.getAttribute('id');
+  console.log(`current mode is now: ${currentMode}`);
+}
+
+// Function for creating a random RGB value
+function getRandomRGB() {
+  let r = Math.floor(Math.random() * Math.floor(256));
+  let g = Math.floor(Math.random() * Math.floor(256));
+  let b = Math.floor(Math.random() * Math.floor(256));
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+// Clears the grid by assigning the base background color to all DOM elements stored
+// in the HTML Collection Array
+function clearGrid () {
+  squares.forEach(square => {
+    square.removeEventListener('mouseenter', paintSquare);
+    square.style.backgroundColor = 'rgb(228, 166, 125)';
+    square.addEventListener('mouseenter', paintSquare);
+  });
+}
+
+function removeGrid () {
+  container.textContent = '';
+}
+
+function resizeGrid () {  
+  removeGrid(); 
+  const newSize = prompt('Choose the new size of the grid (Between 1 - 64): ', 32);
+
+  if (newSize == null) {
+    alert('Please choose a size. The grid will be set to 32');
+    createGrid(32);
+  } else if (newSize <= 0 || newSize > 64) {
+    alert('Please choose a number between 1 and 64');
+    createGrid(32);
+  } else {
+    createGrid(newSize);
+  }
+}
+
+createGrid(squareNumber); 
+
+const modeButtons = document.querySelectorAll('.mode');
+modeButtons.forEach(button => button.addEventListener('click', setMode));
+
+newBtn.addEventListener('click', resizeGrid);
+clearBtn.addEventListener('click', clearGrid);
